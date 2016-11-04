2830195f354eb09b5bb7118886afb91856eb919a
Alejandro <capucha1990@gmail.com>
2016-11-04 17:09:54 +0100

    prueba

---
+++ src/miproducto/miempresa/com/Helloworld.java
@@ -0,0 +1,26 @@
+/**
+ * Este programa....
+ */
+package miproducto.miempresa.com;
+
+/**
+ * @author usuario
+ *
+ */
+public class Helloworld {
+	
+	
+	char separador = '0';
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String contadorString = "tonto";
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola: " + contadorString);
+
+	}
+
+}
